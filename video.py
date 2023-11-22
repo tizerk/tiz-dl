08a028a50d9c9c50007bfd0da18f85ceca8a4b26
Jay Joo <jayjoo0727@gmail.com>
2023-11-22 15:09:54 -0800

    Video format selector

--- video.py
+++ video.py
@@ -1,4 +1,6 @@
 import yt_dlp
+import ffmpeg
+import os
 
 def video(url, ydl, ydl_opts):
     ydl_opts = {
@@ -30,6 +32,21 @@ def video(url, ydl, ydl_opts):
     else:
         ydl = yt_dlp.YoutubeDL({**ydl_opts, "format": f"bestvideo[width<={chosen_resolution}]+bestaudio/best"})
     ydl.download(url)
+    print("Download Complete!")
+
+    result = ydl.extract_info(url, download=False)
+
+    chosen_format = format_choice()
+
+    input_file = f"{result["title"]}.{result["ext"]}"
+
+    output_file = f"{result["title"]}{chosen_format}"
+    if (chosen_format.lower() != f".{result["ext"]}"):
+        ffmpeg.input(input_file).output(output_file).run()
+        os.remove(input_file)
+        print("Conversion Complete!")
+    else:
+        print("No Conversion Needed!")
 
 def resolution_choice(formats):
     print("Available Resolution Options:")
@@ -46,3 +63,24 @@ def resolution_choice(formats):
         print("Incorrect Input: Enter the Integer Value Next to the Desired Resolution\n")
         resolution_choice(formats)
     return formats[choice - 1]['width']
+
+def format_choice():
+    print("Available Format Options:")
+    count = 0
+    ext_formats = [".WEBM", ".MP4", ".MOV", ".WMV", ".FLV", ".AVI", ".MKV", ".3GP", ".M4A"]
+    for i in ext_formats:
+        count += 1
+        print(f"{count}: {i}")
+    try:
+        choice = int(input("Select Desired Format: "))
+    except ValueError:
+        print(
+            "Incorrect Input: Enter the Integer Value Next to the Desired Resolution\n"
+        )
+        format_choice(ext_formats)
+    if 0 >= choice or choice > count:
+        print(
+            "Incorrect Input: Enter the Integer Value Next to the Desired Resolution\n"
+        )
+        format_choice(ext_formats)
+    return ext_formats[choice - 1]
